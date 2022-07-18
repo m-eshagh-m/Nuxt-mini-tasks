10b5df45e37934a023777a0bfc905a2516b4f11a
Mojahed <m.eshagh.m@gmail.com>
2022-07-18 20:22:17 +0430

    Import productstable componnet and fetch data
    by asyncData, data fetched and then send to
    products table component as prop

---
+++ pages/products.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <ProductsTable :products="products" />
+  </v-row>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ProductsPage',
+
+  // fetch data
+  async asyncData() {
+    const { data } = await axios.get('https://fakestoreapi.com/products/')
+    return { products: data }
+  },
+}
+</script>
