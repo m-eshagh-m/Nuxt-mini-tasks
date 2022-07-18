33ec4eede9c69fc9f1b764b6c179c93173c782a6
Mojahed <m.eshagh.m@gmail.com>
2022-07-18 20:50:37 +0430

    Create ArticlesList component for showing articles as list

---
+++ components/ArticlesList.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h1>Articles list link</h1>
+    <div>
+      <ul>
+        <li v-for="article in articles" :key="article.id" class="item">
+          <nuxt-link :to="'/' + article.id">{{ article.title }}</nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    articles: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+
+  li a {
+    color: rgb(15, 140, 223);
+    text-decoration: none;
+  }
+}
+</style>
